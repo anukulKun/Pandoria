0357a1ee6b3a14e6fd0a636dd80546f670c9e7d5
anukulKun <anukulpandit30s@gmail.com>
2026-02-20 15:00:00 +0530

    components - nav, foot, landing

---
+++ src/components/Header/LandingNav.tsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex items-center justify-between px-14 py-3">
+      <div className="flex items-center gap-20 text-sm font-semibold ">
+        <div>COLLECTIONS</div>
+        <div>ABOUT</div>
+      </div>
+      <div className="flex items-center text-3xl font-black">
+        <div>PANDORIA</div>
+      </div>
+      <div className="flex items-center gap-20 text-sm font-semibold ">
+        <div>SEARCH</div>
+        <button
+          onClick={() => navigate("/auth")}
+          className="bg-black text-white py-2.5 px-8 font-normal hover:bg-gray-800 cursor-pointer"
+        >
+          LOG IN
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Header/Navbar.tsx
@@ -0,0 +1,23 @@
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between px-14 py-3">
+      <div className="flex items-center gap-20 text-sm font-semibold ">
+        <div>COLLECTIONS</div>
+        <div>ABOUT</div>
+      </div>
+      <div className="flex items-center text-3xl font-black">
+        <div>PANDORIA</div>
+      </div>
+      <div className="flex items-center gap-20 text-sm font-semibold ">
+        <div>SEARCH</div>
+        <button className="bg-black text-slate-200 py-2.5 px-8 font-normal hover:bg-gray-800 cursor-pointer">
+          LOG IN
+        </button>
+        {/* extra div here */}
+        {/* heres the key test */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
